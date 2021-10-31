20d963915636631baec740aad55efd8fef3588f6
xin.he <xin.he@insolu.cn>
2021-10-31 13:35:43 +0800

    Initial commit

---
+++ src/main/java/com/he/DeptDemoApplication.java
@@ -0,0 +1,13 @@
+package com.he;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeptDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeptDemoApplication.class, args);
+    }
+
+}
